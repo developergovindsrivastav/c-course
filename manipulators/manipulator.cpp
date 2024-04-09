4610ac86ca559414764437bd42fd4917018358de
developergovindsrivastav <govindsrivastav800@gmail.com.com>
2024-04-09 06:52:02 +0530

    c++

---
+++ manipulators/manipulator.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    const int a=10;
+    cout<<setw(10)<<setfill('*')<<a<<endl;
+}
---
+++ variables&constant/constant.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+// we use "const" keyword in c++ to declare a constant variable
+using namespace std;
+int main(){
+    const int a=10;
+    cout<<a<<endl;
+}
